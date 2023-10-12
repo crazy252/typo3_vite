0ba1e708a836c78060fe9e2c73bc11716d54ffec
crazy252 <crazy252.cg@gmail.com>
2023-10-12 10:28:42 +0200

    Version 1.1.6 and support for typo3 v10

--- ext_emconf.php
+++ ext_emconf.php
@@ -8,10 +8,10 @@ $EM_CONF[$_EXTKEY] = [
     'author_email' => 'crazy252.cg@gmail.com',
     'state' => 'stable',
     'clearCacheOnLoad' => 1,
-    'version' => '1.1.5',
+    'version' => '1.1.6',
     'constraints' => [
         'depends' => [
-            'typo3' => '11.0.0-12.4.99'
+            'typo3' => '10.0.0-12.4.99'
         ],
         'conflicts' => [],
         'suggests' => [],
